75f5164227be520d87406858ebeab11e872beb29
HamidrezaHodaei <hodaeiihamidreza@gmail.com>
2026-08-19 17:13:22 +0330

    Add loop to log task titles and completion status

---
+++ loop.js
@@ -0,0 +1,7 @@
+const tasks = [ 
+    {title:'shopping',completed:false},
+    {title:'cleaning',completed:true},
+]
+for (const task of tasks){
+    console.log(`Task: ${task.title}, Completed: ${task.completed}`);
+}
